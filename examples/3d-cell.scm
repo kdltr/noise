094bf00bed0d826b18263dbdc4eee561bca5f3ee
Alex Charlton <alex.n.charlton@gmail.com>
2014-08-22 20:19:22 -0400

    Improved 3D examples

--- examples/3d-cell.scm
+++ examples/3d-cell.scm
@@ -3,28 +3,57 @@
      noise)
 
 ;;; VAO data
-(define vertex-data (f32vector -1 -1
-                                1 -1
-                                1  1
-                               -1  1))
+(define vertex-data (f32vector 0 0 0
+                               1 0 0
+                               1 1 0
+                               0 1 0
+                               0 0 1
+                               1 0 1
+                               1 1 1
+                               0 1 1))
 
 (define index-data (u16vector 0 1 2
-                              0 2 3))
+                              2 3 0
+                              7 6 5
+                              5 4 7
+                              0 4 5
+                              5 1 0
+                              1 5 6
+                              6 2 1
+                              2 6 7
+                              7 3 2
+                              3 7 4
+                              3 4 0))
 
-(define time (f32vector 0))
+;;; Matrices
+(define projection-matrix
+  (perspective 640 480 0.1 100 45))
+
+(define view-matrix
+  (look-at 1.5 2 2
+           0.5 0.5 0.5
+           0 1 0))
+
+(define model-matrix (mat4-identity))
+
+(define mvp (m* projection-matrix
+                (m* view-matrix model-matrix)
+                #t ; Matrix should be in a non-GC'd area
+                ))
 
 (define-pipeline simple-shader
-  ((#:vertex input: ((vertex #:vec2))
-             output: ((pos #:vec2))) 
+  ((#:vertex input: ((vertex #:vec3))
+             uniform: ((mvp #:mat4))
+             output: ((pos #:vec3))) 
    (define (main) #:void
-     (set! gl:position (vec4 vertex 0.0 1.0))
+     (set! gl:position (* mvp (vec4 vertex 1.0)))
      (set! pos vertex)))
-  ((#:fragment input: ((pos #:vec2))
-               uniform: ((time #:float))
+  ((#:fragment input: ((pos #:vec3))
                output: ((frag-color #:vec4))
                use: (cell-noise-3d))
    (define (main) #:void
-     (let ((n #:float (- 1 (* (cell-noise (vec3 (* pos 16) time))
+     (set! jitter 0.7)
+     (let ((n #:float (- 1 (* (cell-noise (* pos 16))
                               1))))
        (set! frag-color (vec4 n n n 1.0))))))
 
@@ -36,21 +65,21 @@
 ;;; Initialization and main loop
 (glfw:with-window (480 480 "Example" resizable: #f)
   (gl:init)
+  (gl:enable gl:+depth-test+)
+  (gl:depth-func gl:+less+)
   (compile-pipelines)
   (let* ((vao (make-vao vertex-data index-data
-                        `((,(pipeline-attribute 'vertex simple-shader) float: 2))))
+                        `((,(pipeline-attribute 'vertex simple-shader) float: 3))))
          (renderable (make-simple-shader-renderable
                       n-elements: (u16vector-length index-data)
                       element-type: (type->gl-type ushort:)
                       vao: vao
-                      time: time)))
+                      mvp: mvp)))
     (let loop ()
       (glfw:swap-buffers (glfw:window))
       (gl:clear (bitwise-ior gl:+color-buffer-bit+ gl:+depth-buffer-bit+))
       (render-simple-shader renderable)
       (gl:check-error)
       (glfw:poll-events)
-      (f32vector-set! time 0 (/ (glfw:get-time)
-                                5))
       (unless (glfw:window-should-close (glfw:window))
         (loop)))))
--- examples/4d-simplex.scm
+++ examples/4d-simplex.scm
@@ -2,30 +2,61 @@
 (use glls-render (prefix glfw3 glfw:) (prefix opengl-glew gl:) gl-math gl-utils
      noise)
 
-;;; VAO data
-(define vertex-data (f32vector -1 -1
-                               1 -1
-                               1  1
-                               -1  1))
-
-(define index-data (u16vector 0 1 2
-                              0 2 3))
-
 (define time (f32vector 0))
 
+;;; VAO data
+(define vertex-data (f32vector 0 0 0
+                               1 0 0
+                               1 1 0
+                               0 1 0
+                               0 0 1
+                               1 0 1
+                               1 1 1
+                               0 1 1))
+
+(define index-data (u16vector 0 1 2
+                              2 3 0
+                              7 6 5
+                              5 4 7
+                              0 4 5
+                              5 1 0
+                              1 5 6
+                              6 2 1
+                              2 6 7
+                              7 3 2
+                              3 7 4
+                              3 4 0))
+
+;;; Matrices
+(define projection-matrix
+  (perspective 640 480 0.1 100 45))
+
+(define view-matrix
+  (look-at 1.5 2 2
+           0.5 0.5 0.5
+           0 1 0))
+
+(define model-matrix (mat4-identity))
+
+(define mvp (m* projection-matrix
+                (m* view-matrix model-matrix)
+                #t ; Matrix should be in a non-GC'd area
+                ))
+
 (define-pipeline simple-shader
-  ((#:vertex input: ((vertex #:vec2))
-             output: ((pos #:vec2)))
+  ((#:vertex input: ((vertex #:vec3))
+             uniform: ((mvp #:mat4))
+             output: ((pos #:vec3)))
    (define (main) #:void
-     (set! gl:position (vec4 vertex 0.0 1.0))
+     (set! gl:position (* mvp (vec4 vertex 1.0)))
      (set! pos vertex)))
-  ((#:fragment input: ((pos #:vec2))
+  ((#:fragment input: ((pos #:vec3))
                uniform: ((time #:float))
                output: ((frag-color #:vec4))
                use: (simplex-noise-4d))
    (begin
      (define (main) #:void
-       (let ((n #:float (+ 0.5 (* 0.5 (fractal-snoise (vec4 pos time 0)
+       (let ((n #:float (+ 0.2 (* 0.3 (fractal-snoise (vec4 pos time)
                                                       5 1 0.5 2 0.5)))))
          (set! frag-color (vec4 n n n 1.0)))))))
 
@@ -37,13 +68,16 @@
 ;;; Initialization and main loop
 (glfw:with-window (480 480 "Example" resizable: #f)
   (gl:init)
+  (gl:enable gl:+depth-test+)
+  (gl:depth-func gl:+less+)
   (compile-pipelines)
   (let* ((vao (make-vao vertex-data index-data
-                        `((,(pipeline-attribute 'vertex simple-shader) float: 2))))
+                        `((,(pipeline-attribute 'vertex simple-shader) float: 3))))
          (renderable (make-simple-shader-renderable
                       n-elements: (u16vector-length index-data)
                       element-type: (type->gl-type ushort:)
                       vao: vao
+                      mvp: mvp
                       time: time)))
     
     (let loop ()
